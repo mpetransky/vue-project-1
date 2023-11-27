b8c110419e08caeddb6bb7578adc7ff49157cf2c
Martin Petransky <mpetransky@gmail.com>
2023-11-27 14:35:14 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,7 @@
+<template>
+  <HelloWorld msg="You did it!" />
+</template>
+
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="greetings">
+    <h3>
+      Hello world!
+      <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
+      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
+    </h3>
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  msg: {
+    type: String,
+    required: true
+  }
+})
+</script>
